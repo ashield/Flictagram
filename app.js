0ccc42a610221e06e1b7fa3cfcd6f81faea05e43
As <andrewshield99@hotmail.com>
2013-11-05 18:44:03 +1300

    Five
    To review with Sunil. Need to work on DRY concept and tidy up the js,
    most functionality is there now.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
       }
     });
 $('#loading').hide();
-$('.paginate').hide();
+// $('.paginate').hide();
 $('#container').hide().fadeIn(2000);
 
 }); // End of document ready  
@@ -21,16 +21,13 @@ $('#container').hide().fadeIn(2000);
 
 
 function submit(){
-
 // Generic setup
-$('#container').animate({'padding-top': '50px'});
+$('#container').animate({'padding-top':'50px'});
 $('#tag').blur();
 $('#loading').show();
 
-
 // Remove previous search results
- $("#images img").remove();
-
+ $(".pics").remove();
 
 // Storing user input
 var tag = document.getElementById("tag").value;
@@ -38,7 +35,6 @@ var tag = document.getElementById("tag").value;
 // INSTAGRAM
 var instagramKey = "259340442.1fb234f.bde60c19506746cfbc82b20953dd222d"
 var fetchInstagram = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + instagramKey + "&count=10" + "&callback=?";
-
 
 function getInstagram (){
 $.ajax({
@@ -54,11 +50,10 @@ function instagram (instagram) {
 	for (i=0; i < instagram.data.length ; i++) {
 	var instaImg = instagram.data[i].images.standard_resolution.url;
 	var instaLink = instagram.data[i].link;
-	var instagramImage = "<div><a href=" + instaLink + " " + "target='_blank'><img src=" + instaImg + "></a></div>";
+	var instagramImage = "<div class='pics'><a href=" + instaLink + " " + "target='_blank'><img src=" + instaImg + "></a></div>";
 	$("#images").append(instagramImage).hide().delay('2000').fadeIn('slow');
 
-
-	} // end of for loop
+} // end of for
 
 $(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
@@ -68,41 +63,31 @@ $(window).scroll(function() {
 	var nextMaxId = instagram.pagination.next_max_tag_id;
 	var getMoreInstagram = fetchInstagram + '&max_tag_id' + '=' + nextMaxId;
 
-	$.ajax({
-
+$.ajax({
   dataType: "jsonp",
-  url: getMoreInstagram, 
+  url: getMoreInstagram,
   success: function (instagram) {
 	for (i=0; i < instagram.data.length ; i++) {
 	var instaImg = instagram.data[i].images.standard_resolution.url;
 	var instaLink = instagram.data[i].link;
-	var instagramImage = "<div><a href=" + instaLink + " " + "target='_blank'><img src=" + instaImg + "></a></div>";
+	var instagramImage = "<div class='pics'><a href=" + instaLink + " " + "target='_blank'><img src=" + instaImg + "></a></div>";
 	$("#images").append(instagramImage).delay('2000').fadeIn('slow');
 
 } // end of for loop
 } // End of second Instagram function
 }); // end of Ajax
-console.log(getMoreInstagram);
-   }
+	console.log(getMoreInstagram);
+  }
 }); // end of window
-
-
-
-
-
 
 } // End of Instagram function
 
 console.log(fetchInstagram);
 
-// $("#tag").val('');
- 
-
 // FLICKR
 var flickrKey = "f7a9edaaeec15c4bf0055f74d5105a6d"
 var flickrPageNumber = 1
 var fetchFlickr = "http://api.flickr.com/services/rest/?api_key=" + flickrKey + "&format=json&jsoncallback=?&tags=" + tag + "&per_page=10&page=" + flickrPageNumber + "&method=flickr.photos.search"
-
 
 function getFlickr (){
 $.ajax({
@@ -114,18 +99,17 @@ $.ajax({
 
 getFlickr ()
 
-	function flickr (flickr) {
+function flickr (flickr) {
   //build the url of the Flickr photo
   	for (f=0; f < flickr.photos.photo.length ; f++) {
     var flickrImg = 'http://farm' + flickr.photos.photo[f].farm + '.static.flickr.com/' + flickr.photos.photo[f].server + '/' + flickr.photos.photo[f].id + '_' + flickr.photos.photo[f].secret + '_z.jpg'
     var flickrLinks = "http://www.flickr.com/photos/" + flickr.photos.photo[f].owner + "/" + flickr.photos.photo[f].id
-    var flickrImage = "<div><a href=" + flickrLinks + " " + "target='_blank'><img src=" + flickrImg + "></a></div>";
+    var flickrImage = "<div class='pics'><a href=" + flickrLinks + " " + "target='_blank'><img src=" + flickrImg + "></a></div>";
     $("#images").append(flickrImage).hide().delay('2000').fadeIn('slow');
 
 }// end of for
 
 	console.log(fetchFlickr)
-
 
 $(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
@@ -141,7 +125,7 @@ $(window).scroll(function() {
 	for (f=0; f < flickr.photos.photo.length ; f++) {
     var flickrImg = 'http://farm' + flickr.photos.photo[f].farm + '.static.flickr.com/' + flickr.photos.photo[f].server + '/' + flickr.photos.photo[f].id + '_' + flickr.photos.photo[f].secret + '_z.jpg'
     var flickrLinks = "http://www.flickr.com/photos/" + flickr.photos.photo[f].owner + "/" + flickr.photos.photo[f].id
-       var flickrImage = "<div><a href=" + flickrLinks + " " + "target='_blank'><img src=" + flickrImg + "></a></div>";
+       var flickrImage = "<div class='pics'><a href=" + flickrLinks + " " + "target='_blank'><img src=" + flickrImg + "></a></div>";
     $("#images").append(flickrImage).delay('2000').fadeIn('slow');
 
 } //end of for
@@ -152,24 +136,18 @@ console.log (getMoreFlickr);
    }
 }); // end of window
 
-
 } // end of Flickr function
-
-
-
 
 // $('.paginate').show();
 
-
-
 } // End of Submit function
 
-// Strip spaces on submit (hack)
+// Strip spaces (hack)
 function stripspaces(input)
 {
   input.value = input.value.replace(/\s/gi,"");
   return true;
-}
+};
 
 
 	
